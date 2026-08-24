d8824feae4dcc54575fbe18b8b7a87d9bbbfeaf6
Nada Momtaz <136115414+nadamomtaz@users.noreply.github.com>
2026-08-24 23:42:47 +0300

    Implement employee onboarding and department management
    Added methods for onboarding employees, adding departments, and managing employee skills.

--- Sprint3/Services/Company.cs
+++ Sprint3/Services/Company.cs
@@ -14,5 +14,68 @@ namespace Sprint3
         Queue<Employee> Onboarding = new Queue<Employee>();
         Stack<string> ActionHistory = new Stack<string>();
         HashSet<string> skills = new HashSet<string>();
+        public event Eventhandler<EmployeeEventArgs> EmployeeOnboarded;
+        public Result<Employee> AddEmployee(Employee employee)
+                        {
+                            Onboarding.Enqueue(employee);
+                            ActionHistory.Push($"{employee.Name} added to Onboarding");
+                        
+                            return new Result<Employee>
+                            {
+                                Success = true,
+                                Message = $"{employee.Name} added to onboarding queue.",
+                                Data = employee
+                            };
+                        }
+        public void AddtoActiveEmployee()
+        {
+            if (Onboarding.Count == 0)
+            {
+                Console.WriteLine(" No employee onboarding");
+                return;
+            }
+
+            Employee e1 = Onboarding.Dequeue();
+            ActiveEmployee.Add(e1);
+            ActionHistory.Push($"{e1.Name} removed from Onboarding && added to Active Employee");
+
+        }
+public Result<Department> AddNewDepartment(Department department)
+{
+    if (Departments.ContainsKey(department.Id))
+    {
+        return new Result<Department>
+        {
+            Success = false,
+            Message = $"Department with Id {department.Id} already exists.",
+            Data = null
+        };
     }
+
+    Departments.Add(department.Id, department);
+    ActionHistory.Push($"{department.Name} added to Departments");
+
+    return new Result<Department>
+    {
+        Success = true,
+        Message = $"{department.Name} added",
+        Data = department
+    };
+}
+        public void AddEmployeeSkills(string skill, Employee employee)
+        {
+            employee.skills.Add(skill);
+            skilles.Add(skill);
+            ActionHistory.Push($" {skill} added to{employee.Name}");
+        }
+         public void AddEmployeSkills(string skill, Employee employee)
+        {
+            employee.skills.Add(skill);
+            skilles.Add(skill);
+            ActionHistory.Push($" {skill} added to{employee.Name}");
+        }
+        
+        
+    }
+    
 }
