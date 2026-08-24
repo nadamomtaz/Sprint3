4c872600fa29365b5b8cb80c0512e017e3326c18
Nada Momtaz <136115414+nadamomtaz@users.noreply.github.com>
2026-08-24 22:01:54 +0300

    Add Employee model with properties

---
+++ Sprint3/Models/Employee.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint3
+{
+    internal class Employee
+    {
+        public int id { get; set; }
+        public string? Name { get; set; }
+        public int DepartmentId { get; set; }
+        public DateTime HireDate{ get; set; }
+        public decimal Salary { get; set; }
+        
+        //Id, Name, HireDate, DepartmentId, Salary
+    }
+}
