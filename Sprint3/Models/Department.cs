88b8ad6350c559547e9f66baf705b61dcf21c7e8
Nada Momtaz <136115414+nadamomtaz@users.noreply.github.com>
2026-08-24 22:10:24 +0300

    Add Department class with id and Name properties

---
+++ Sprint3/Models/Department.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint3
+{
+    internal class Department
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+    }
+}
